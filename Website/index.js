941d79e258031564008c573c9ece81bac57f3694
nehabinish <nehabinish24@gmail.com>
2024-06-28 15:25:28 +0200

    Intro Image changed

--- Website/index.js
+++ Website/index.js
@@ -17,7 +17,7 @@ var granimInstance = new Granim({
     direction: 'top-bottom',
     isPausedWhenNotInView: true,
     image : {
-        source: 'assets/snow.jpg', //change image for intro section if desired
+        source: 'assets/intro_image.jpg', //change image for intro section if desired
         blendingMode: 'multiply',
     },
     states : {
